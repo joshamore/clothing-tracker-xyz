0e2d84bf6392e3a8f5130f86899997c1a24eb292
Josh Amore <joshamore31@gmail.com>
2021-07-18 07:54:11 +1000

    Add input container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from './components/Navbar';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+
+const CoreContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+`;
+
+const InputContainer = styled(Paper)`
+  margin-top: 16px;
+  width: 300px;
+  border-radius: 16px;
+`;
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <p>Hello</p>
-    </div>
+      <CoreContainer>
+        <InputContainer elevation={1}>
+          <p>HELP</p>
+        </InputContainer>
+      </CoreContainer>
+    </>
   );
 };
 
