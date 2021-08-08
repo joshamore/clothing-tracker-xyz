8eedc107443e455c9f277cc49dc0009f651925f9
Josh Amore <joshamore31@gmail.com>
2021-08-08 19:41:51 +1000

    Add handling for session loading state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useState, useEffect } from 'react';
 import { supabase } from './helpers/supabaseClient';
+import Spinner from '@material-ui/core/CircularProgress';
 import Login from './pages/Login';
 
 const App = () => {
-  const [session, setSession] = useState(null);
+  const [session, setSession] = useState({ loading: true, data: null });
 
-  console.log(session);
   useEffect(() => {
-    setSession(supabase.auth.session());
+    setSession({ loading: false, data: supabase.auth.session() });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
+      setSession({ loading: false, data: session });
     });
   }, []);
 
+  // Render spinner while session is loading
+  if (session.loading) return <Spinner />;
+
   return (
     <div>
-      {!session ? (
+      {!session.data ? (
         <Login />
       ) : (
         <>
