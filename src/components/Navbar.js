be18ce1778094ce832fc7ab7b8192fb202e9c700
Josh Amore <joshamore31@gmail.com>
2021-08-09 20:18:45 +1000

    Add logout button to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { supabase } from '../helpers/supabaseClient';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
@@ -10,17 +13,35 @@ const MenuButton = styled(IconButton)`
   margin-left: auto;
 `;
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn }) => {
   return (
     <AppBar position='static'>
       <Toolbar>
         <Typography variant='h6'>👕 Clothing Tracker</Typography>
         <MenuButton edge='start' color='inherit' aria-label='menu'>
-          <MenuIcon />
+          {isLoggedIn ? (
+            <Button
+              variant='contained'
+              color='secondary'
+              onClick={() => supabase.auth.signOut()}
+            >
+              Sign Out
+            </Button>
+          ) : (
+            <MenuIcon />
+          )}
         </MenuButton>
       </Toolbar>
     </AppBar>
   );
+};
+
+Navbar.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+Navbar.defaultProps = {
+  isLoggedIn: false,
 };
 
 export default Navbar;
