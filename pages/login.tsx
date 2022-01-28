9413875c8222e33027883c90c29e140d9794517b
Josh Amore <joshamore31@gmail.com>
2022-01-28 11:47:32 +1100

    Refactor login page UI to use MUI standard components

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,52 @@
 import { useState } from "react";
+import styled from "@emotion/styled";
+import { toast } from "react-toastify";
+
 import { supabase } from "../src/helpers/supabaseClient";
 import { SupabaseError as ApiError } from "../src/helpers/types";
-import { toast } from "react-toastify";
+
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 import CoreLayout from "../src/components/CoreLayout";
+
+const CoreContainer = styled(Container)`
+	display: flex;
+	justify-content: center;
+`;
+
+const LoginCard = styled(Paper)`
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	margin-top: 16px;
+	padding: 8px 0;
+	width: 300px;
+	border-radius: 16px;
+	h2 {
+		margin: 0 0 8px 0;
+		width: 100%;
+	}
+`;
+
+const SubheaderText = styled(Typography)`
+	width: 100%;
+	margin: 0 0 8px 0;
+`;
+
+const LoginInputContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	margin-bottom: 8px;
+`;
+
+const LoginInput = styled(TextField)`
+	margin: 0 8px;
+`;
 
 const Login = () => {
 	const [loading, setLoading] = useState<boolean>(false);
@@ -24,27 +68,39 @@ const Login = () => {
 
 	return (
 		<CoreLayout isLoggedIn={false}>
-			<h1>Log In (Existing user)</h1>
-			<p>Log in via magic link with your email below</p>
-			<div>
-				<input
-					type="email"
-					placeholder="Your email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-			</div>
-			<div>
-				<button
-					onClick={(e) => {
-						e.preventDefault();
-						handleLogin(email);
-					}}
-					disabled={loading}
-				>
-					{loading ? <span>Loading</span> : <span>Send magic link</span>}
-				</button>
-			</div>
+			<CoreContainer>
+				<LoginCard>
+					<Typography variant="h5" component="h2" align="center">
+						Log In (Existing user)
+					</Typography>
+					<SubheaderText align="center">
+						Log in via magic link with your email below.
+					</SubheaderText>
+
+					<LoginInputContainer>
+						<LoginInput
+							id="login-email-input"
+							fullWidth
+							variant="outlined"
+							label={email ? "" : "Email"}
+							InputLabelProps={{ shrink: false }}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</LoginInputContainer>
+
+					<Button
+						variant="contained"
+						disabled={loading}
+						onClick={(e) => {
+							e.preventDefault();
+							handleLogin(email);
+						}}
+					>
+						{loading ? <span>Loading...</span> : <span>Send magic link</span>}
+					</Button>
+				</LoginCard>
+			</CoreContainer>
 		</CoreLayout>
 	);
 };
