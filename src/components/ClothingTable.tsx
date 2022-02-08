11dc500e202cbebedd12b77908a6735d445d1e76
joshamore <joshamore31@gmail.com>
2022-02-08 12:18:55 +1100

    Add sort ability to view table

--- src/components/ClothingTable.tsx
+++ src/components/ClothingTable.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import styled from "@emotion/styled";
 import dayjs from "dayjs";
 
 import { ClothingItemType } from "../helpers/types";
-import { getClothingTypeNameFromId } from "../helpers/utils";
+import {
+	getClothingTypeNameFromId,
+	capitalizeFirstLetter,
+} from "../helpers/utils";
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -22,23 +26,106 @@ interface ClothingTableProps {
 	clothingItems: ClothingItemType[];
 }
 
+interface SortByState {
+	field: string;
+	direction: "asc" | "desc";
+}
+
 const ClothingTable = ({ clothingItems }: ClothingTableProps) => {
-	// Removing retired clothing items
-	const renderClothingItems = clothingItems.filter((item) => !item.is_retired);
+	const [sortBy, setSortBy] = useState<SortByState>({
+		field: "name",
+		direction: "asc",
+	});
+	const [renderClothingItems, setRenderClothingItems] = useState<
+		ClothingItemType[]
+	>(clothingItems.filter((item) => !item.is_retired));
+
+	/**
+	 * Handles sorting of the clothing items table when the user selects a column.
+	 *
+	 * @param {string} field The field to sort by.
+	 * @param {boolean} isDate Confirm if compare field is a date.
+	 */
+	const handleSort = (field: string, isDate: boolean = false) => {
+		// Getting new sort direction.
+		const newDirection =
+			sortBy.field === field
+				? sortBy.direction === "asc"
+					? "desc"
+					: "asc"
+				: "asc";
+
+		const sortedClothingItems = [...renderClothingItems];
+
+		if (newDirection === "asc") {
+			sortedClothingItems.sort((a, b) => {
+				const fieldA = isDate ? new Date(a[field]) : a[field];
+				const fieldB = isDate ? new Date(b[field]) : b[field];
+
+				if (fieldA < fieldB) {
+					return -1;
+				} else if (fieldA > fieldB) {
+					return 1;
+				} else {
+					return 0;
+				}
+			});
+		} else {
+			sortedClothingItems.sort((a, b) => {
+				const fieldA = isDate ? new Date(a[field]) : a[field];
+				const fieldB = isDate ? new Date(b[field]) : b[field];
+
+				if (fieldA > fieldB) {
+					return -1;
+				} else if (fieldA < fieldB) {
+					return 1;
+				} else {
+					return 0;
+				}
+			});
+		}
+
+		setSortBy({ field, direction: newDirection });
+		setRenderClothingItems(sortedClothingItems);
+	};
 
 	return (
 		<TableContainer component={Paper}>
 			<Table aria-label="clothing items table">
 				<TableHead>
 					<TableRow>
-						<TableHeaderCell>Name</TableHeaderCell>
-						<TableHeaderCell>Purchase Date</TableHeaderCell>
-						<TableHeaderCell>Clothing Type</TableHeaderCell>
+						<TableHeaderCell>
+							<TableSortLabel
+								onClick={() => handleSort("name")}
+								active={sortBy.field === "name"}
+								direction={sortBy.direction}
+							>
+								Name
+							</TableSortLabel>
+						</TableHeaderCell>
+						<TableHeaderCell>
+							<TableSortLabel
+								onClick={() => handleSort("purchase_date")}
+								active={sortBy.field === "purchase_date"}
+								direction={sortBy.direction}
+							>
+								Purchase Date
+							</TableSortLabel>
+						</TableHeaderCell>
+						<TableHeaderCell>
+							<TableSortLabel
+								onClick={() => handleSort("clothing_type", true)}
+								active={sortBy.field === "clothing_type"}
+								direction={sortBy.direction}
+							>
+								Clothing Type
+							</TableSortLabel>
+						</TableHeaderCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
 					{renderClothingItems.map((row) => (
-						<TableRow key={row.id}>
+						<TableRow key={`table-row-${row.id}`}>
 							<TableCell component="th" scope="row">
 								<Link href={`/item/${row.id}`}>{row.name}</Link>
 							</TableCell>
@@ -46,7 +133,9 @@ const ClothingTable = ({ clothingItems }: ClothingTableProps) => {
 								{dayjs(row.purchase_date).format("DD/MM/YYYY")}
 							</TableCell>
 							<TableCell component="th" scope="row">
-								{getClothingTypeNameFromId(row.clothing_type)}
+								{capitalizeFirstLetter(
+									getClothingTypeNameFromId(row.clothing_type)
+								)}
 							</TableCell>
 						</TableRow>
 					))}
